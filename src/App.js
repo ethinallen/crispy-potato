7b73502cb97c814306b5b7164907e20d5c941f01
Drew Emery <andrewbemery@gmail.com>
2021-07-28 13:09:44 -0700

    in this together

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We're all in this together :)
         </a>
       </header>
     </div>
